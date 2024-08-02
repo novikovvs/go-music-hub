a8b038ef621a73a6ab9c7122ba2bf73252c98959
novikovvs <vladonnx@mail.ru>
2024-08-02 03:10:22 +0300

    Fix models.ts

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,34 +1,29 @@
 export namespace main {
-
+	
 	export class Track {
-
-
+	
+	
 	    static createFrom(source: any = {}) {
 	        return new Track(source);
 	    }
-
+	
 	    constructor(source: any = {}) {
 	        if ('string' === typeof source) source = JSON.parse(source);
-
+	
 	    }
 	}
 	export class TrackLibrary {
-
-
+	
+	
 	    static createFrom(source: any = {}) {
 	        return new TrackLibrary(source);
 	    }
-
+	
 	    constructor(source: any = {}) {
 	        if ('string' === typeof source) source = JSON.parse(source);
-
+	
 	    }
 	}
 
 }
-
-
-
-
-
 
