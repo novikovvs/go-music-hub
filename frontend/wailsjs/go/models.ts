383c99d3df2c9e010781e7a8512ebcbcbf974cbb
novikovvs <vladonnx@mail.ru>
2024-08-02 03:09:05 +0300

    Player and queue are created

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,19 +1,17 @@
 export namespace main {
-
+	
 	export class TrackLibrary {
-
-
+	
+	
 	    static createFrom(source: any = {}) {
 	        return new TrackLibrary(source);
 	    }
-
+	
 	    constructor(source: any = {}) {
 	        if ('string' === typeof source) source = JSON.parse(source);
-
+	
 	    }
 	}
 
 }
 
-export class TrackLibrary {
-}
